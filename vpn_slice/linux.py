1addef79b9a68d298ee95f3131487e906d425ff9
Dan Lenski <dlenski@gmail.com>
2019-10-18 10:38:30 -0700

    to create tunnel device, use os functions rather than subprocess

--- vpn_slice/linux.py
+++ vpn_slice/linux.py
@@ -94,9 +94,10 @@ class IptablesProvider(FirewallProvider):
 
 class CheckTunDevProvider(TunnelPrepProvider):
     def create_tunnel(self):
-        if not os.path.exists('/dev/net/tun'):
-            subprocess.check_call(['mkdir', '-p', '/dev/net'])
-            subprocess.check_call(['mknod', '-m', '0640', '/dev/net/tun', 'c', '10', '200'])
+        node = '/dev/net/tun'
+        if not os.path.exists(path):
+            os.makedirs(os.path.dirname(node), exist_ok=True)
+            os.mknod(node, mode=0o640 | os.stat.S_IFCHR, device = os.makedev(10, 200))
     def prepare_tunnel(self):
         if not os.access('/dev/net/tun', os.R_OK | os.W_OK):
             raise OSError("can't read and write /dev/net/tun")
