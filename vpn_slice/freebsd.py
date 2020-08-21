3a23de6b62c6a147397de13a08bce7b62a05e216
Jens Heinrich <59469646+JensHeinrich@users.noreply.github.com>
2020-08-21 08:55:46 +0200

    Create freebsd.py
    Add proper procfs handling for FreeBSD

---
+++ vpn_slice/freebsd.py
@@ -0,0 +1,22 @@
+import os
+import subprocess
+import stat
+
+from .posix import PosixProcessProvider
+from .util import get_executable
+
+
+class ProcfsProvider(PosixProcessProvider):
+    def pid2exe(self, pid):
+        try:
+            return os.readlink('/proc/%d/file' % pid)
+        except (OSError, IOError):
+            return None
+
+    def ppid_of(self, pid=None):
+        if pid is None:
+            return os.getppid()
+        try:
+            return int(next(open('/proc/%d/status' % pid)).split()[3])
+        except (OSError, ValueError, IOError):
+            return None
