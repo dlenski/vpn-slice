dlenski/vpn-slice

commit f9a8fce1821ba28b4c5a56842f6fc18227404a72

Adds pr #78 and a split DNS provider for Systemd-resolved

vpn_slice/linux.py
@@ -1,9 +1,11 @@
 import os
 import stat
 import subprocess
+import re
+import logging
 
 from .posix import PosixProcessProvider
-from .provider import FirewallProvider, RouteProvider, TunnelPrepProvider
+from .provider import FirewallProvider, RouteProvider, TunnelPrepProvider, SplitDNSProvider
 from .util import get_executable
 
 
@@ -109,3 +111,51 @@ class CheckTunDevProvider(TunnelPrepProvider):
     def prepare_tunnel(self):
         if not os.access('/dev/net/tun', os.R_OK | os.W_OK):
             raise OSError("can't read and write /dev/net/tun")
+
+class ResolveConfSplitDNSProvider(SplitDNSProvider):
+    def __init__(self):
+        self.resolvconf = get_executable('/usr/bin/resolvconf')
+
+    def _resolvconf(self, *args):
+        cl = [self.resolvconf]
+        cl.extend(args)
+        subprocess.check_call(cl)
+
+    def configure_domain_vpn_dns(self, domains, nameservers, tundev):
+        cl = [self.resolvconf]
+        cl.extend(['-p', '-a', tundev])
+
+        p = subprocess.Popen(cl, stdin=subprocess.PIPE)
+
+        p.stdin.write("search ".encode('utf-8'))
+
+        for domain in domains:
+            p.stdin.write ((domain+" ").encode('utf-8'))
+        p.stdin.write("\n".encode('utf-8'))
+
+        for nameserver in nameservers:
+            p.stdin.write(("nameserver " + str(nameserver) + "\n").encode('utf-8'))
+
+        p.stdin.close()
+
+class ResolvedSplitDNSProvider(SplitDNSProvider):
+    def __init__(self):
+        self._inuse = re.match("^/run/systemd/resolve/", os.readlink('/etc/resolv.conf')) != None
+        self.resolvectl = get_executable('/usr/bin/resolvectl')
+
+    def inuse(self):
+        self._inuse
+
+    def _resolvectl(self, *args):
+        cl = [self.resolvectl]
+        cl.extend(args)
+        from pprint import pprint
+        pprint(cl)
+        subprocess.check_call(cl)
+
+    def configure_domain_vpn_dns(self, domains, nameservers, tundev):
+        try:
+            self._resolvectl(*([ 'domain', tundev ] + [ x.encode('utf-8') for x in domains ]))
+            self._resolvectl(*([ 'dns', tundev ] + [ str(x).encode('utf-8') for x in nameservers ]))
+        except Exception as e:
+            logging.error('Error at %s', 'resolvectl', exc_info=e)

vpn_slice/__main__.py
@@ -30,8 +30,10 @@ def get_default_providers():
         DNSPythonProvider = None
 
     if platform.startswith('linux'):
-        from .linux import CheckTunDevProvider, Iproute2Provider, IptablesProvider, ProcfsProvider
+        from .linux import CheckTunDevProvider, Iproute2Provider, IptablesProvider, ProcfsProvider, ResolveConfSplitDNSProvider, ResolvedSplitDNSProvider
         from .posix import DigProvider, PosixHostsFileProvider
+        if not ResolvedSplitDNSProvider.inuse:
+            ResolvedSplitDNSProvider = None
         return dict(
             process = ProcfsProvider,
             route = Iproute2Provider,
@@ -39,6 +41,7 @@ def get_default_providers():
             dns = DNSPythonProvider or DigProvider,
             hosts = PosixHostsFileProvider,
             prep = CheckTunDevProvider,
+            domain_vpn_dns = ResolvedSplitDNSProvider or ResolveConfSplitDNSProvider,
         )
     elif platform.startswith('darwin'):
         from distutils.version import LooseVersion
@@ -147,7 +150,7 @@ def do_disconnect(env, args):
 
     if args.vpn_domains is not None:
         try:
-            providers.domain_vpn_dns.deconfigure_domain_vpn_dns(args.vpn_domains, env.dns)
+            providers.domain_vpn_dns.deconfigure_domain_vpn_dns(args.vpn_domains, env.dns, env.tundev)
         except OSError:
             print("WARNING: failed to deconfigure domains vpn dns", file=stderr)
 
@@ -242,7 +245,10 @@ def do_connect(env, args):
         if 'domain_vpn_dns' not in providers:
             print("WARNING: no split dns provider available; can't split dns", file=stderr)
         else:
-            providers.domain_vpn_dns.configure_domain_vpn_dns(args.vpn_domains, env.dns)
+            try:
+                providers.domain_vpn_dns.configure_domain_vpn_dns(args.vpn_domains, env.dns, env.tundev)
+            except:
+                print("WARNING: failed to configure domains vpn dns", file=stderr)
 
 
 def do_post_connect(env, args):
@@ -336,7 +342,7 @@ def do_post_connect(env, args):
 ########################################
 
 # Translate environment variables which may be passed by our caller
-# into a more Pythonic form (these are take from vpnc-script)
+# into a more Pythonic form (these are taken from vpnc-script)
 reasons = Enum('reasons', 'pre_init connect disconnect reconnect attempt_reconnect')
 vpncenv = [
     ('reason', 'reason', lambda x: reasons[x.replace('-', '_')]),
@@ -451,7 +457,7 @@ def parse_args_and_env(args=None, environ=os.environ):
     g = p.add_argument_group('Nameserver options')
     g.add_argument('--no-ns-hosts', action='store_false', dest='ns_hosts', default=True, help='Do not add nameserver aliases to /etc/hosts (default is to name them dns0.tun0, etc.)')
     g.add_argument('--nbns', action='store_true', dest='nbns', help='Include NBNS (Windows/NetBIOS nameservers) as well as DNS nameservers')
-    g.add_argument('--domains-vpn-dns', dest='vpn_domains', default=None, help="comma separated domains to query with vpn dns")
+    g.add_argument('--domains-vpn-dns', dest='vpn_domains', default=None, help="comma separated domains to query with VPN DNS")
     g = p.add_argument_group('Debugging options')
     g.add_argument('--self-test', action='store_true', help='Stop after verifying that environment variables and providers are configured properly.')
     g.add_argument('-v', '--verbose', default=0, action='count', help="Explain what %(prog)s is doing. Specify repeatedly to increase the level of detail.")

vpn_slice/mac.py
@@ -115,7 +115,7 @@ class BSDRouteProvider(RouteProvider):
 
 
 class MacSplitDNSProvider(SplitDNSProvider):
-    def configure_domain_vpn_dns(self, domains, nameservers):
+    def configure_domain_vpn_dns(self, domains, nameservers, tundev):
         if not os.path.exists('/etc/resolver'):
             os.makedirs('/etc/resolver')
         for domain in domains:
@@ -124,7 +124,7 @@ class MacSplitDNSProvider(SplitDNSProvider):
                 for nameserver in nameservers:
                     resolver_file.write("nameserver {}\n".format(nameserver))
 
-    def deconfigure_domain_vpn_dns(self, domains, nameservers):
+    def deconfigure_domain_vpn_dns(self, domains, nameservers, tundev):
         for domain in domains:
             resolver_file_name = "/etc/resolver/{0}".format(domain)
             if os.path.exists(resolver_file_name):

vpn_slice/provider.py
@@ -145,14 +145,14 @@ class TunnelPrepProvider:
         """
 
 class SplitDNSProvider:
-    def configure_domain_vpn_dns(self, domains, nameservers):
+    def configure_domain_vpn_dns(self, domains, nameservers, tundev):
         """Configure domain vpn dns.
 
         Base class behavior is to do nothing.
 
         """
 
-    def deconfigure_domain_vpn_dns(self, domains, nameservers):
+    def deconfigure_domain_vpn_dns(self, domains, nameservers, tundev):
         """Remove domain vpn dns.
 
         Base class behavior is to do nothing.

commit 9703bce49e0996f43dba2e4fcc295bceb06ebdb5

Bugfix ResolvedSplitDNSProvider.inuse()
In addition to being unnecessarily complex (regex… WHY?), this breaks on
systems where `/etc/resolv.conf` is *not* a symlink.

This has been broken since f9a8fce1821ba28b4c5a56842f6fc18227404a72.

vpn_slice/linux.py
@@ -1,7 +1,6 @@
 import os
 import stat
 import subprocess
-import re
 import logging
 
 from .posix import PosixProcessProvider
@@ -142,7 +141,10 @@ class ResolveConfSplitDNSProvider(SplitDNSProvider):
 class ResolvedSplitDNSProvider(SplitDNSProvider):
     @staticmethod
     def inuse():
-        return re.match("^/run/systemd/resolve/.*", os.readlink('/etc/resolv.conf')) != None
+        try:
+            return os.readlink('/etc/resolv.conf').startswith('/run/systemd/resolve/')
+        except OSError:
+            return False  # not in use if it's not a symlink
 
     def __init__(self):
         self.resolvectl = get_executable('/usr/bin/resolvectl')